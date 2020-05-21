87342a78f8b03fbc0b8ec99a2d800b6f6767eac7
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-05-21 19:56:36 +0200

    Update ajax.js

--- Modul-3/M3-W6-D2-Ajax/ajax.js
+++ Modul-3/M3-W6-D2-Ajax/ajax.js
@@ -16,3 +16,31 @@ ajaxRequest('http://someurl.com', function myCallBack(err, data) {
      console.log(data);
       }
    });
+
+   // Callback Hell
+   getUser ('URL', (user) => {
+
+    getBlogPosts('URL' + user.name, (blogPosts) => {
+      
+      getComments('URL' + blogpost[0], (comments) => {
+        console.log(comments);
+
+      })
+    })
+   });
+
+   // PROMISES - A wrapper around CallBacks
+
+   // a promise can have 3 stages:
+
+   /*
+   pending - You don't have any result yet. You are waiting for it. 
+   resolved - everything went fine, and you got the value back.
+   rejected -you get something back, but that something is an error.
+   */
+
+   /* How to create a Promise - we will learn it later
+   const promise = new Promise((resolve, reject) => {
+
+   });
+   */
