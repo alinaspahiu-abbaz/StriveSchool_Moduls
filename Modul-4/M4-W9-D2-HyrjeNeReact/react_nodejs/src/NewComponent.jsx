b8504827845f1b6ed67f13050c0591aebb21e981
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-04 21:45:32 +0200

    Create NewComponent.jsx

---
+++ Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/NewComponent.jsx
@@ -0,0 +1,26 @@
+// Functional Component --> PROPS
+// Class Based Component --> props, state, lifecycle methods
+
+// import React from 'react'
+
+// const NewComponent = (props) => {
+//     console.log('props in newcomponent', props)
+//     return (
+//     <div>
+//         <ul>
+//           {
+//             props.elements.map(
+//               (element) => 
+//                 (
+//                   <li> {element} </li>
+//                 )
+//               )
+//           }
+//         </ul>
+//     </div> 
+//     )
+
+
+// }
+
+// export default NewComponent
