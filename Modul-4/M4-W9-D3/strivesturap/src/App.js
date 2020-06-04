c2ebec4be0772c6dae9943ad7fb5974679f15de1
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-04 21:47:18 +0200

    Day 3

---
+++ Modul-4/M4-W9-D3/strivesturap/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavBar from "./components/Navbar"
+import Home from "./components/Home"
+
+
+function App() 
+{
+  return(
+           <div className="App">
+            
+                 <NavBar title="Strivesturant" />
+                 <Home />
+             
+           </div>
+         );
+}
+
+export default App;
+// App.js
+// Navbar.jsx
+// Home.jsx
+// menu.json
---
+++ Modul-4/M4-W9-D3/strivesturap/src/components/DishComments.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class DishComments extends React.Component {
+    render () {
+        return (array && array.length)
+    }
+}
