54e445fdbc062dbda3246a3e4cce322b8f72d61c
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-04 21:45:12 +0200

    Update App.js

--- Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/App.js
+++ Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/App.js
@@ -2,26 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './MyComponent';
+import NewComponent from './NewComponent';
+import UseState from './UseState'
 
-function App() {
+function App() { 
   return (
     //this is a JSX
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1> Hello World!</h1>
+       
+       
         <h3> This is my first test!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NewComponent elements={['one', 'two', 'three']}/>
+
+        <UseState elements={['nje', 'dy', 'tre']}/>
+
+        
 
         <MyComponent text="some text"> </MyComponent>
 
