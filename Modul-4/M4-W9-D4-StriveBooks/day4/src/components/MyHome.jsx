e60007fdb8a3ee56c5d4de4728bd44d3e776832f
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-09 21:09:33 +0200

    global variables

---
+++ Modul-4/M4-W9-D4-StriveBooks/day4/src/components/MyHome.jsx
@@ -0,0 +1,9 @@
+let bookCategories=['fantasy', 'horror', 'history', 'fantasy', 'scifi']
+
+let books = {
+  fantasy: require('../data/fantasy.json'),
+  horror: require('../data/horror.json'),
+  romance: require('../data/romance.json'),
+  history: require('../data/history.json'),
+  scifi: require('../data/scifi.json')
+}
