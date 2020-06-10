12b86d4ff6256402a23c8feca21d866f914f0540
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-10 22:32:48 +0200

    Create App.js

---
+++ Modul-4/M4-W9-D5/strivesturap/src/App.js
@@ -0,0 +1,15 @@
+  
+import React from "react";
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <NavBar title="Strivesturant" />
+      <Home />
+    </>
+  );
+}
+
+export default App;
