400bbe64617f2de1fd5f3db891527354509f9c1d
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-11 21:57:27 +0200

    Create NavBar.jsx

---
+++ Modul-4/Week 10/M4-W10-D3-/netflix/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Navbar, Nav, Image} from 'react-bootstrap';
+
+
+const NavBar = () => (
+    <Navbar variant='dark' expand="lg">
+    <Navbar.Brand href="#home">
+    <Image src='assets/logo.png' style={{width:'100px', height:'55px'}}></Image></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      
+    </Nav>
+    <div className="d-flex my-3 my-lg-0">
+        <i className="fa fa-search icons mr-3"></i>
+        <div id="kids">KIDS</div>
+        <i className="fa fa-bell icons mx-3"></i>
+        <i className="fa fa-user icons mx-3"></i>
+      </div>
+
+    
+  </Navbar.Collapse>
+</Navbar>
+)
+
+export default NavBar;
