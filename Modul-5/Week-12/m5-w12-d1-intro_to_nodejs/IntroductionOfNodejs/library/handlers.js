70678a26510a298965fcf0552af4607679e78ab8
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-22 22:07:14 +0200

    Update handlers.js

--- Modul-5/Week-12/m5-w12-d1-intro_to_nodejs/IntroductionOfNodejs/library/handlers.js
+++ Modul-5/Week-12/m5-w12-d1-intro_to_nodejs/IntroductionOfNodejs/library/handlers.js
@@ -1,1 +1,6 @@
+const getUsers =() => {
+    const usersList = [
+        {id:1, name:'Diego'}, 
+        {id:2, name:'Tobia'}
+    ]
     return JSON.stringify(usersList)// turning the objeckt into Json string
