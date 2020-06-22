2c8267b80b5f597a23c2ebcf5382fb5fd13b811d
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-22 21:14:37 +0200

    Create server.js

---
+++ Modul-5/Week-12/m5-w12-d1-intro_to_nodejs/IntroductionOfNodejs/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World! This is Spartaaaaaaa!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Serverrrr!!! runninggggg at http://${hostname}:${port}/`);
+});
