eb9f101b11f6679b5cbc95fc8cefd13eea5bd820
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-22 22:06:45 +0200

    Update server.js

--- Modul-5/Week-12/m5-w12-d1-intro_to_nodejs/IntroductionOfNodejs/server.js
+++ Modul-5/Week-12/m5-w12-d1-intro_to_nodejs/IntroductionOfNodejs/server.js
@@ -1,13 +1,26 @@
 const http = require('http');
+const {parse} = require('url');
+const { response } = require('express');
 const handler = require('./library/handlers') //importing the file
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! This is Spartaaaaaaa!');
+ const parsedUrl = parse(req.url, true)
+console.log(req.url)
+console.log(parsedUrl)
+
+switch(parsedUrl.pathname){
+  case '/': //here e definojm pathin.
+    res.end("Main page!")
+    break
+  case '/about':
+    res.end("About page!")
+    break
+  case '/users':
+    handler(req, res)
+}
 });
 
 server.listen(port, hostname, () => {
