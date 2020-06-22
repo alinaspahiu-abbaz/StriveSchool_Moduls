caa4a22f3b2e83b1c46d5fef9f16dff46fafcfc8
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-22 22:07:32 +0200

    Update handlers.js

--- Modul-5/Week-12/m5-w12-d1-intro_to_nodejs/IntroductionOfNodejs/library/handlers.js
+++ Modul-5/Week-12/m5-w12-d1-intro_to_nodejs/IntroductionOfNodejs/library/handlers.js
@@ -4,3 +4,23 @@ const getUsers =() => {
         {id:2, name:'Tobia'}
     ]
     return JSON.stringify(usersList)// turning the objeckt into Json string
+}
+
+const sendRes = (res, statusCode, headers, data) => {
+    res.writeHead(statusCode, headers)
+    res.end(data)
+
+}
+module.exports = (req, res) => {
+    switch (req.method) {
+        case 'GET':
+            // retrieve list of users 'from db'
+            // send the list as response
+            sendRes(res, 200, {'Content-Type':'application/json'}, getUsers())
+            break
+        case "POST":
+            sendRes()
+            break
+        default:
+    }
+}
