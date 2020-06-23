9feca84bc31c9b4c59f67d077830c3f4b01c0012
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-23 10:13:03 +0200

    The template

--- Modul-5/Week-12/m5-w12-d2/src/services/users/index.js
+++ Modul-5/Week-12/m5-w12-d2/src/services/users/index.js
@@ -1,10 +1,17 @@
 /*
 
-1. get all users data on url: localhost:3001/users Get
-2. get single users's data on url: localhost:3001/users/:ie GET
+1. get all users's data on url: localhost:3001/users Get
+2. get single users's data on url: localhost:3001/users/:id GET
 3. create single user record on url: localhost:3001/users POST
-4. modify single users's data on url: localhost:3001/users/:id PUT
+4. modify single user's data on url: localhost:3001/users/:id PUT
 5. delete single user's data on url: localhost:3001/users/:id DELETE
 */
 
 const express = require("express")
+
+const router = express.Router()
+
+router.get();
+router.post()
+
+module.exports = router
