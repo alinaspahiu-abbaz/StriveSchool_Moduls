39e11a8eadd274ccd36be8bdc6c704a710199556
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-23 11:42:39 +0200

    Update index.js

--- Modul-5/Week-12/m5-w12-d2/src/services/users/index.js
+++ Modul-5/Week-12/m5-w12-d2/src/services/users/index.js
@@ -9,10 +9,14 @@
 
 const express = require("express")//hired party module
 const fs = require('fs')// core module dedicated to file system interactions
+const uniqid = require('uniqid')
 const path = require("path") //core module
 
 const router = express.Router()
 // Route number 1.: the one who gets users data
+
+const usersFilePath = path.join(__dirname, 'users.json')
+
 router.get("/", (request, response)=>{ //(request, response) => is the handler for this specific route
 
     // a) retrive users list from a file on disk(bc we dont have a reale database yet.)
@@ -20,7 +24,7 @@ router.get("/", (request, response)=>{ //(request, response) => is the handler f
    // const fileContent = fs.readFileSync()
    // console.log(__dirname + '\\users.json')// avoid
    // console.log(path.join(__dirname, 'users.json')) // use path.join
-    const usersFilePath = path.join(__dirname, 'users.json') //we composed the path on disk
+    //const usersFilePath = path.join(__dirname, 'users.json') //we composed the path on disk
     const fileContentAsABuffer = fs.readFileSync(usersFilePath)// please read the file (we are getting a Buffer back) 
     //console.log(fileContentAsABuffer.toString())
     const fileContent = fileContentAsABuffer.toString()// we need to translate the buffer into something human readable
@@ -32,11 +36,48 @@ router.get("/", (request, response)=>{ //(request, response) => is the handler f
 
 
 //Route number 2.
-router.get("/:id", (request, response)=>{ })
+// retrive onluy single user data from a file on dissk and send it back
+
+router.get("/:id", (request, response)=>{ 
+
+      // a. read the file on disk and get back an array of users
+    
+    const fileContentAsABufer = fs.readFileSync(usersFilePath)
+    const usersArray = JSON.parse(fileContentAsABufer.toString())
+    console.log(usersArray)
+
+      // b. filter out the array to retrive the specified user(we're gonna be using id to retrive the unique user)
+    
+    console.log("ID: ", request.params.id)
+    const user = usersArray.filter((user)=> user.id=== parseInt(request.params.id))
+    console.log(user)
+
+      // c. send the user back into the response
+      response.send(user)
+ })
 
 
 //Route number 3.
-router.post("/", (request, response)=>{ })
+router.post("/", (request, response)=>{ 
+    console.log(request.body)
+    const newUser = {...request.body, id: uniqid()}
+
+    //a. read the content of the file and get back an array
+    const fileContentAsABufer = fs.readFileSync(usersFilePath)
+    const usersArray = JSON.parse(fileContentAsABufer.toString())
+
+    // b. adding the new element(in this case user) to the array
+    const newId = uniqid()
+    usersArray.push(newUser)
+  //  console.log(usersArray)
+    // c. writing the new content into the same file
+    fs.writeFileSync(usersFilePath), JSON.stringify(usersArray)
+
+    // d. respondeing with the status 201 which means "created"
+    response.send(newUser)
+
+
+})
 
 
 //Route number 4.
