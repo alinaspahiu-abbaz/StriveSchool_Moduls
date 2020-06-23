e4f5299a917002bafe3342b857bc5ba94d1b0372
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-23 10:42:50 +0200

    Update server.js

--- Modul-5/Week-12/m5-w12-d2/src/server.js
+++ Modul-5/Week-12/m5-w12-d2/src/server.js
@@ -1,8 +1,11 @@
 //the entred point. this is gonna be executed first
 const express = require("express")
+const usersRoutes = require('./services/users')
 
 const server = express()
 
+server.use("/users", usersRoutes)
+
 server.listen(3001, () => {
-    console.log('serverrrr is running on port 3001!')
+    console.log('server is running on port 3001!')
 })
