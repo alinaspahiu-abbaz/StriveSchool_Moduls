35118d8d19728cfeee1510ff8bc959be157b2b46
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-23 11:42:36 +0200

    Update server.js

--- Modul-5/Week-12/m5-w12-d2/src/server.js
+++ Modul-5/Week-12/m5-w12-d2/src/server.js
@@ -4,6 +4,8 @@ const usersRoutes = require('./services/users')
 
 const server = express()
 
+server.use(express.json())// we are telling express parse the body when they are in json format
+
 server.use("/users", usersRoutes)
 
 server.listen(3001, () => {
