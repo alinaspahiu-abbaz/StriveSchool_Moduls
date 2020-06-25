50c644934f9a4296f3194297d75332f39c97c96d
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-25 22:53:43 +0200

    Update index.js

---
+++ Modul-5/Week-12/m5-w12-d4/backend/m5-w12-d3/src/services/files/index.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const multer = require("multer")
+const fs=require('fs-extra')
+const {join} = require("path")
+const { writeFile } = require("fs")
+
+const router = express.Router()
+
+const upload = multer({})
+
+const studentsFolderPath = join(__dirname, "../../public/img/students")
+//1. upload one file
+router.post('/upload', upload.single("avatar"), async(req, res, next) => {
+    //req.files<-- here is where we're gonna find the file
+    //console.log("console",req.file.buffer)
+    //console.log(studentsFolderPath)
+    try{
+        await fs.writeFile(join(studentsFolderPath, req.file.originalname), req.file.buffer )
+
+    }catch(error) {
+        console.log(error)
+    }
+    res.send("ok")
+
+})
+
+//2. upload multiple files
+router.post(
+    '/uploadMultiple', 
+    upload.array("multipleAvatar"), 
+    async (req, res, next) => {
+        try{
+    //console.log(req.files)
+
+    const arrayOfPromises = req.files.map(file => writeFile(join(studentsFolderPath, file.originalname), file.buffer))
+
+    await Promise.all(arrayOfPromises)
+    res.send("ok")} catch(error){
+        console.log(error)
+    }
+   
+    
+})
+
+//downloadnpm
+router.get('/upload', (req, res, next) => {
+    
+})
+
+module.exports = router
