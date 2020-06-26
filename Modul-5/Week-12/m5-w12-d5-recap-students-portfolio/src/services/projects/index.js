2a66cda8c42c959669c40d1ede804ba446ed5681
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-26 21:08:57 +0200

    Create index.js

---
+++ Modul-5/Week-12/m5-w12-d5-recap-students-portfolio/src/services/projects/index.js
@@ -0,0 +1,37 @@
+const express = require ("express")
+
+const projectRouter = express.Router()
+
+// -- CRUD for Students: --
+
+
+projectRouter.get("/")
+
+//projectRouter.get()
+
+//projectRouter.post()
+
+//projectRouter.put()
+
+//projectRouter.delete()
+
+
+
+
+///---------------------
+// Upload Files for project
+
+//projectRouter.post()
+
+
+
+
+//----------------
+// Check Email:
+
+// projectRouter.get()
+
+
+
+
+module.exports = projectRouter
