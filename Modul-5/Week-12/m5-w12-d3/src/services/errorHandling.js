dc074f5ee58260204ddfed50c5aac6801a082dec
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-27 23:06:15 +0200

    Update errorHandling.js

--- Modul-5/Week-12/m5-w12-d3/src/services/errorHandling.js
+++ Modul-5/Week-12/m5-w12-d3/src/services/errorHandling.js
@@ -1,38 +1,22 @@
-
-const notFoundHandler=(err, req, res, next)=> {
-    if(err.httpStatusCode === 404) {
-        res.status(404).send("Resourcce not found!")
-    } 
-    next(err)
+const notFoundHandler = (err, req, res, next) => {
 
 }
 
 const unauthorizedHandler = (err, req, res, next) => {
-    if(err.httpStatusCode === 401) {
-        res.status(404).send("unauthorized!!")
-    } 
-    next(err)
 
 }
+
 const forbiddenHandler = (err, req, res, next) => {
-    if(err.httpStatusCode === 403) {
-        res.status(404).send("Forbidden!")
-    } 
-    next(err)
-    
+
 }
 
-const catchAllHandler = (err, req, res, next) => {
-    if(!res.headersSent){
-    res.status(err.httpStatusCode || 500).send(err.message)
-    }
+const catchAllHandler=(err, req, res, next) =>(
 
-    
-}
+)
 
 module.exports = {
     notFoundHandler,
     unauthorizedHandler,
     forbiddenHandler,
-    catchAllHandler,
+    catchAllHandler
 }
