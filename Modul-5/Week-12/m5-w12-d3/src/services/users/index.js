7591c7f6c99f63e1e5e1aef70cfc94ca87c0657c
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-27 21:05:53 +0200

    Update index.js

--- Modul-5/Week-12/m5-w12-d3/src/services/users/index.js
+++ Modul-5/Week-12/m5-w12-d3/src/services/users/index.js
@@ -11,65 +11,20 @@ const readFile = (fileName) => {
   return JSON.parse(fileContent)
 }
 
-router.get("/:id", (req, res, next) => {
-  try{
-  const usersDB = readFile("users.json")
-  const user = usersDB.filter((user) => user.ID === req.params.id)
-  res.send(user)
-  }
-  catch(error) {
-    next(error)
-  }
-})
-
 router.get("/", (req, res) => {
   const usersDB = readFile("users.json")
-  if (req.query && req.query.name) {
-    const filteredUsers = usersDB.filter(
-      (user) =>
-        user.hasOwnProperty("name") &&
-        user.name.toLowerCase() === req.query.name.toLowerCase()
-    )
-    res.send(filteredUsers)
-  } else {
-    res.send(usersDB)
-  }
+
+  if(req.query && req.query.name){
+    const filteredUsers = usersDB.filter
+    (user => user.hasOwnProperty("name")
+     && user.name===req.query.name )
+
+     res.send("ok")
+     console.log(filteredUsers)
+   } 
+   
+   else{ res.send(usersDB) 
+            console.log(usersDB)}
 })
-
-router.post("/", (req, res) => {
-  const usersDB = readFile("users.json")
-  const newUser = {
-    ...req.body,
-    ID: uniqid(),
-    createdAt: new Date(),
-  }
-
-  usersDB.push(newUser)
-
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(usersDB))
-
-  res.status(201).send(usersDB)
-})
-
-router.delete("/:id", (req, res) => {
-  const usersDB = readFile("users.json")
-  const newDb = usersDB.filter((x) => x.ID !== req.params.id)
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDb))
-
-  res.send(newDb)
-})
-
-router.put("/:id", (req, res) => {
-  const usersDB = readFile("users.json")
-  const newDb = usersDB.filter((x) => x.ID !== req.params.id) //removing previous item
-  const users = req.body
-  users.ID = req.params.id
-  newDb.push(users) //adding new item
-  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDb))
-
-  res.send(newDb)
-})
-
-router.get("/whatever", (req, res) => {})
 
 module.exports = router
