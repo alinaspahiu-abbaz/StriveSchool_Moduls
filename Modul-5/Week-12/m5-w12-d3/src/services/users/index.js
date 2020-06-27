35445c12f5b16e1fda08733c46ac1ba0f294ae8d
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-27 22:42:03 +0200

    Update index.js

--- Modul-5/Week-12/m5-w12-d3/src/services/users/index.js
+++ Modul-5/Week-12/m5-w12-d3/src/services/users/index.js
@@ -29,10 +29,49 @@ router.get("/", (req, res) => {
 
 
 
-// GET all
+// GET 
 
 router.get("/:idu", (req, res) => {
   // GET http://localhost:3001/users/12
   
+  const usersDB = readFile("users.json")
+  const retrivedUser = usersDB.filter((user) => user.ID === req.params.idu)
+  res.send(retrivedUser)
+
+})
+
+// POST
+
+router.post("/", (req, res) => {
+  // POST http://localhost:3001/users/
+  
+  const usersDB = readFile("users.json")
+  const newUser = {...req.body, ID: uniqid(), createdAt: new Date() }
+
+  usersDB.push(newUser)
+
+  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(usersDB))
+  res.status(201).send(newUser)
+
+})
+
+// PUT
+router.put("/:idu", (req, res) => {
+  const usersDB = readFile("users.json")
+  const newDB = usersDB.filter((user) => user.ID !== req.params.idu)
+  const modifiedUser = {...req.body, ID: req.params.idu}
+  newDB.push(modifiedUser)
+  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDB))
+
+  res.send(newDB)
+
+})
+
+// DELETE
+router.delete("/:idu", (req, res) => {
+  const usersDB = readFile("users.json")
+  const newDB = usersDB.filter((user) => user.ID !== req.params.idu)
+  fs.writeFileSync(path.join(__dirname, "users.json"), JSON.stringify(newDB))
+  res.send(newDB)
 })
 module.exports = router
