8d6ddddd76c5cbe19a666377f24355af08d15c65
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-27 23:08:33 +0200

    Update server.js

--- Modul-5/Week-12/m5-w12-d3/src/server.js
+++ Modul-5/Week-12/m5-w12-d3/src/server.js
@@ -2,11 +2,13 @@ const express = require("express")
 const listEndpoints = require("express-list-endpoints")
 const usersRouter = require("./services/users")
 const moviesRouter = require("./services/movies")
+const problematicRoutes = require("./services/problematicRoutes")
 
  
 const server = express()
 
 const port = process.env.PORT || 3002
+
 
 const loggerMiddleware = (req, res, next) =>{ //next - is a function that sents record to the next element in the chain
   console.log(`Logged url: ${req.url}| method: ${req.method} --date: ${new Date()}`)
@@ -14,12 +16,17 @@ const loggerMiddleware = (req, res, next) =>{ //next - is a function that sents 
 }
 // mund ta perdorim per te gjithe routerat. ose vete per nje specifik,
 //
+
+
 server.use(express.json()) // Built in middleware
-// server.use(loggerMiddleware)
+server.use(loggerMiddleware)
+
+
 
 // ROUTES
 server.use("/users", usersRouter)
 server.use("/movies", loggerMiddleware, moviesRouter)// dhe kjo vlen vetem per movies
+server.use("/problems", problematicRoutes)
 
 console.log(listEndpoints(server))
 server.listen(port, () => {
