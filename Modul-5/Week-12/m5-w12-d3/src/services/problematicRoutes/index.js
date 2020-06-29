8a1c27d521c8009d0151804395fdd6ebee1142be
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-06-29 13:02:50 +0200

    Update index.js

--- Modul-5/Week-12/m5-w12-d3/src/services/problematicRoutes/index.js
+++ Modul-5/Week-12/m5-w12-d3/src/services/problematicRoutes/index.js
@@ -7,4 +7,25 @@ router.get("/houston", (req, res) => {
   throw new Error("Houston we have a problem!")
 })
 
+router.get("/notExistant", (req, res, next) => {
+
+  let err = new Error("I cannot find it!")
+  err.httpStatusCode = 404
+  next(err) //pass the err as a parameter
+})
+
+router.get("/forbiddenRoute", (req, res, next) => {
+
+  let err = new Error("Forbidden!")
+  err.httpStatusCode = 403
+  next(err) //pass the err as a parameter
+})
+
+router.get("/anotherProblem", (req, res, next) => {
+
+  let err = new Error("ERROOOOORRRR!!")
+  
+  next(err) //pass the err as a parameter
+})
+
 module.exports = router
