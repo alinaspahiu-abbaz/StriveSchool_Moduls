f8e8bea0af865a8a255d81e3241a311c40dba512
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-09 22:47:54 +0200

    Update index.js

--- Modul-5/Week-12/m5-w12-d4/backend/m5-w12-d3/src/services/files/index.js
+++ Modul-5/Week-12/m5-w12-d4/backend/m5-w12-d3/src/services/files/index.js
@@ -2,14 +2,16 @@ const express = require("express")
 const multer = require("multer")
 const fs=require('fs-extra')
 const {join} = require("path")
-const { writeFile } = require("fs")
+const { writeFile } = require("fs-extra")
 
 const router = express.Router()
 
 const upload = multer({})
 
 const studentsFolderPath = join(__dirname, "../../public/img/students")
+
 //1. upload one file
+//http://localhost:4002/files/upload
 router.post('/upload', upload.single("avatar"), async(req, res, next) => {
     //req.files<-- here is where we're gonna find the file
     //console.log("console",req.file.buffer)
@@ -30,14 +32,14 @@ router.post(
     upload.array("multipleAvatar"), 
     async (req, res, next) => {
         try{
-    //console.log(req.files)
-
-    const arrayOfPromises = req.files.map(file => writeFile(join(studentsFolderPath, file.originalname), file.buffer))
-
-    await Promise.all(arrayOfPromises)
-    res.send("ok")} catch(error){
-        console.log(error)
-    }
+            const arrayOfPromises = req.files.map( (file) => 
+            writeFile(join(studentsFolderPath, file.originalname), file.buffer))
+        
+            await Promise.all(arrayOfPromises)
+            res.send("OK")
+            }catch(error){
+                console.log(error)
+            }
    
     
 })
