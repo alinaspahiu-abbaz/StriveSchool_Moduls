4860e46b8bcca901e4dc094be6c0ef05a2112e46
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-09 22:49:30 +0200

    Create schema.js

---
+++ Modul-6/m6-w14-d3/src/services/books/schema.js
@@ -0,0 +1,30 @@
+const { Schema } = require("mongoose")
+const mongoose = require("mongoose")
+
+const BookSchema = new Schema(
+  {
+    _id: String,
+    title: String,
+    author: String,
+    description: String,
+    year: Number,
+    genre: Array,
+    price: Number,
+  },
+  { 
+    _id: false // here we say to mongoDB  do not create the id automaticly
+   }
+)
+
+BookSchema.post("save", function(error, doc, next){
+  if(error.name === "MonggoError" && error.code === 11000){
+    const err = new Error("duplicate ker error")
+
+    err.httpStatus = 400
+    next(err)  
+  } else {
+    next()
+  }
+})
+
+module.exports = mongoose.model("Book", BookSchema)
