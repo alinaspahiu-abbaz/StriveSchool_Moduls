e244b9ef2b07c1abd84837db6256936e80524620
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-09 22:48:41 +0200

    Create schema.js

---
+++ Modul-6/m6-w14-d2/src/services/books/schema.js
@@ -0,0 +1,19 @@
+const { Schema } = require("mongoose")
+const mongoose = require("mongoose")
+
+const BookSchema = new Schema(
+  {
+    _id: String,
+    title: String,
+    author: String,
+    description: String,
+    year: Number,
+    genre: Array,
+    price: Number,
+  },
+  { 
+    _id: false // here we say to mongoDB  do not create the id automaticly
+   }
+)
+
+module.exports = mongoose.model("Book", BookSchema)
