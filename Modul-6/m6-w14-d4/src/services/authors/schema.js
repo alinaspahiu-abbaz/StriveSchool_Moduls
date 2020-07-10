4af9a3704f1f8540f39a8542f99d6a02c6dbdf37
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-10 23:57:30 +0200

    Create schema.js

---
+++ Modul-6/m6-w14-d4/src/services/authors/schema.js
@@ -0,0 +1,9 @@
+const {Schema, model} = require("mongoose")
+
+const authorSchema = new Schema({
+    firstName:String,
+    lastName:String, 
+    age:Number
+})
+
+module.exports =model("Authors", authorSchema)
