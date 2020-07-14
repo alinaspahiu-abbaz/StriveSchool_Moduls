f0c1efda8d58da7509df91d0c10c1ff422cc5e1a
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-14 23:13:32 +0200

    Create App.js

---
+++ Modul-6/Week-15/m6-w15-d2/react/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import BookList from './components/BookList';
+
+
+export default class App extends Component {
+  state = {
+    books: []
+  }
+  render() {
+    return (
+      <div>
+        <h1> HEllo world!</h1>
+        <BookList books={this.state.books} />
+      </div>
+    )
+  }
+  
+  componentDidMount = async ()=>{
+    const res = await fetch("http://localhost:3022/books")
+    //http://localhost:3032/students
+    //http:&//localhost:5432/books
+    const books = await res.json()
+    this.setState({
+      books: books
+    })
+  }
+}
+
