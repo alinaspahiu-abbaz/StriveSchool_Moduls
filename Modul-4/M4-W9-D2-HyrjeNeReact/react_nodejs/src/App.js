4908b92c916d7863aee44050a4cbe44b4e63c7ea
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-25 21:53:23 +0200

    Update App.js

--- Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/App.js
+++ Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/App.js
@@ -1,36 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import MyComponent from './MyComponent';
-import NewComponent from './NewComponent';
-import UseState from './UseState'
+
 
 function App() { 
   return (
     //this is a JSX
     <div className="App">
       <header className="App-header">
-        
+        <MyComponent text="Some text" number={3}/>
+        <MyComponent text="other text" />
+        <MyComponent />
         <img src={logo} className="App-logo" alt="logo" />
-       
-       
+           <p>Edit <code> Hiu</code> and save to reload</p>
         <h3> This is my first test!</h3>
-        <NewComponent elements={['one', 'two', 'three']}/>
-
-        <UseState elements={['nje', 'dy', 'tre']}/>
-
-        
-
-        <MyComponent text="some text"> </MyComponent>
-
-        <MyComponent 
-           text="other text"
-           spaceship={6}>
-       </MyComponent>
-
-        <MyComponent></MyComponent>
-
-        <a href="/"> Content </a>
+      <a href="/"> Content </a>
+      <p>Hello Strivers!!</p>
 
       </header>
     </div>
