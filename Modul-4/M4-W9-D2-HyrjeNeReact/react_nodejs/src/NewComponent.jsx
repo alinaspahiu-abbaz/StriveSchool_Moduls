b4a74c5ebb3e0dcfa35d87de42461e91ba2d0d8c
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-26 13:31:52 +0200

    Update NewComponent.jsx

--- Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/NewComponent.jsx
+++ Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/NewComponent.jsx
@@ -1,26 +1,19 @@
 // Functional Component --> PROPS
 // Class Based Component --> props, state, lifecycle methods
 
-// import React from 'react'
+import React from 'react'
 
-// const NewComponent = (props) => {
-//     console.log('props in newcomponent', props)
-//     return (
-//     <div>
-//         <ul>
-//           {
-//             props.elements.map(
-//               (element) => 
-//                 (
-//                   <li> {element} </li>
-//                 )
-//               )
-//           }
-//         </ul>
-//     </div> 
-//     )
+const NewComponent = (props) => {
+    console.log('props in newcomponent', props)
+    return (
+    <div >
+        <h1> My list of elements:</h1>
+         <ul> { props.elements.slice(0,5).map( 
+           (element, i) => ( <li key={i}> {element} </li>)) } </ul>
+    </div> 
+    )
 
 
-// }
+}
 
-// export default NewComponent
+export default NewComponent
