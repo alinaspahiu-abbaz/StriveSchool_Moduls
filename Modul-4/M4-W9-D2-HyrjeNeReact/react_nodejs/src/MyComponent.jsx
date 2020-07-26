fda51b25f44e94227639154d946c5469e2e9737b
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-26 13:31:48 +0200

    Update MyComponent.jsx

--- Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/MyComponent.jsx
+++ Modul-4/M4-W9-D2-HyrjeNeReact/react_nodejs/src/MyComponent.jsx
@@ -1,55 +1,38 @@
-import React from 'react';
-
+import React from "react"
 
 class MyComponent extends React.Component {
 
     state = {
         counter: 0,
-        show:true
     }
 
-    increase = () => {
-        // dont do this --> this.state.counter = this.state.counter + 1 
-        // you can not overWrite the state.
-
+    increase = () =>{
         this.setState({
-            counter: this.state.counter + 1
+            counter: this.state.counter +1
         })
     }
 
-    render() {
-        console.log('PROPS OBJECT', this.props)
-        console.log('State', this.state)
-        return(
-        <div style={{border: '2px solid aqua', marginBottom:'10px' }}>
-            <button onClick={this.increase}> Increase</button>
-            <p> My Counter from the state is {this.state.counter} </p>
-            <h2>This is Number {this.props.spaceship}</h2> 
-            <h3 className="title">{this.props.text || 'No title provided!'}</h3>
-        </div> 
-        )
+    decrease = () =>{
+        this.setState({counter:this.state.counter -1})
     }
+    
+    render(){
+        
+        return(
+        <div>
+            <button onClick={this.decrease}> Decrease </button>
+            <button onClick={this.increase}> Increase </button>
+            
+            <p> My Counter from the state is {this.state.counter} </p>
+            
+            <h1 className="title">{this.props.text || 'No title provided'} 
+           This is number {this.props.number}</h1>
+        </div>
+        )
+        
+    
 
+    }
 }
 
-// const MyComponent = (props) => // functional component
-// {
-
-//     console.log('PROPS OBJECT', props)
-//     return( 
-//        <div style={{border: '2px solid aqua', marginBottom:'10px', padding:'15px' }}>
-//          <h2>This is Number {props.spaceship}</h2> 
-//          <h3 className="title">{props.text || 'No title provided, readdocs!'} 
-//          </h3>
-//        </div> )
-
-
-// }
-
-
 export default MyComponent
-
-// PROPS
-// STATE
-
-// Class based component
