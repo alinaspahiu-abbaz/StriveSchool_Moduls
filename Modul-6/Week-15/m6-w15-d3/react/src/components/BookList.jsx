b83ea371ee122eb02815fb2050c89da40d5029f7
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-27 22:43:51 +0200

    Update BookList.jsx

--- Modul-6/Week-15/m6-w15-d3/react/src/components/BookList.jsx
+++ Modul-6/Week-15/m6-w15-d3/react/src/components/BookList.jsx
@@ -9,14 +9,14 @@ class BookList extends Component {
     render (){
         return(
             <Container className="my-5">
-                <Row>
+                <Row >
                     {this.state.books.slice(0,30).map(x => <BookListItem key={x.asin} item={x} />)}
                 </Row>
             </Container>
         )
     }
     componentDidMount = async ()=>{
-        const res = await fetch("http://localhost:3023/books")
+        const res = await fetch("http://localhost:3024/books")
         //http://localhost:3032/students
         //http:&//localhost:5432/books
         const books = await res.json()
