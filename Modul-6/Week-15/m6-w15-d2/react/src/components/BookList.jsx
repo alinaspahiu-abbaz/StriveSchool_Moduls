e2b47c7298aeafc6509507b5518905c70ff1e064
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-27 22:43:21 +0200

    Update BookList.jsx

--- Modul-6/Week-15/m6-w15-d2/react/src/components/BookList.jsx
+++ Modul-6/Week-15/m6-w15-d2/react/src/components/BookList.jsx
@@ -3,15 +3,26 @@ import {Container, Row} from "react-bootstrap"
 import BookListItem from "./BookListItem"
 
 class BookList extends Component {
+    state = {
+        books: []
+      }
     render (){
         return(
             <Container className="my-5">
                 <Row>
-                    {this.props.books.slice(0,30).map(x=><BookListItem key={x.asin} item={x} />)}
+                    {this.state.books.slice(0,30).map(x => <BookListItem key={x.asin} item={x} />)}
                 </Row>
             </Container>
         )
     }
+    componentDidMount = async ()=>{
+        const res = await fetch("http://localhost:3023/books")
+        //http://localhost:3032/students
+        //http:&//localhost:5432/books
+        const books = await res.json()
+        this.setState({
+          books: books.rows        })
+      }
 }
 
 export default BookList
