cf0c4e23607dd5333f130706702d3a2da0f0b2c3
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-03 23:44:28 +0200

    Create index.js

---
+++ Modul-7/Week-17/m7-w17-d1-hyrjeNeReduxe/hyrje-ne-reduxe/src/store/index.js
@@ -0,0 +1,10 @@
+import {createStore} from "redux";
+import {mainReducer} from "../reducers";
+
+const initialState ={
+    count: 0
+}
+
+export default function configureStore() {
+    return createStore(mainReducer, initialState)
+}
