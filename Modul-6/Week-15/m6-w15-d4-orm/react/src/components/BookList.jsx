91ef00788c0a144b8352b00d62a0f531caa12dfd
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-03 23:42:31 +0200

    Create BookList.jsx

---
+++ Modul-6/Week-15/m6-w15-d4-orm/react/src/components/BookList.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+import {Container, Row} from "react-bootstrap"
+import BookListItem from "./BookListItem"
+
+class BookList extends Component {
+
+    render (){
+        return(
+            <Container className="my-5">
+                <Row >
+                    {this.props.books.map(x => <BookListItem key={x.asin} item={x} />)}
+                </Row>
+            </Container>
+        )
+    }
+  
+}
+
+export default BookList
