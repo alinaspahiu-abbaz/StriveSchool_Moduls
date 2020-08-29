77faabb386a56e5dbb011144f7cf735a12c5f7d9
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-30 00:33:56 +0200

    Update schema.js

--- Modul-6/m6-w14-d2/src/services/books/schema.js
+++ Modul-6/m6-w14-d2/src/services/books/schema.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose")
 
 const BookSchema = new Schema(
   {
-    _id: String,
-    title: String,
-    author: String,
+    _id:  {type: String, required: true},
+    title:       String,
+    author:      String,
     description: String,
-    year: Number,
-    genre: Array,
-    price: Number,
+    year:        Number,
+    genre:       Array,
+    price:       Number,
   },
   { 
     _id: false // here we say to mongoDB  do not create the id automaticly
