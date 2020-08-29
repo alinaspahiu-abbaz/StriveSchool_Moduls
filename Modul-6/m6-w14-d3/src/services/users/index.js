d97496ba86287d865780a2a0f0bfa5eb41f1cf3a
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-30 00:34:15 +0200

    Update index.js

--- Modul-6/m6-w14-d3/src/services/users/index.js
+++ Modul-6/m6-w14-d3/src/services/users/index.js
@@ -6,51 +6,47 @@ const q2m = require("query-to-mongo")
 const usersRouter = express.Router()
 
 
-// 1. GET all:
+// 1. GET all users with queries: //http://localhost:3022/users
+        //a Query example: http://localhost:3022/users?name=Alina&limit=3&sort=-age&offset=4&fields=name&surname=Spahiu
+        // rezults: 
+        // criteria: { name: 'Alina', surname: 'Spahiu' },
+        // options: { fields: {name:1}, sort: {age: -1},skip: 4, limit:3 }    
+        // links: [Function: links]
 usersRouter.get("/", async (req, res, next) => {
  const parsedQuery = q2m(req.query)
  console.log(parsedQuery)
   try{
-      const usersList = await UserSchema.find(
+      const usersList = await UserSchema.find( 
         parsedQuery.criteria,
         parsedQuery.options.fields
       )
       .sort(parsedQuery.options.sort)
       .skip(parsedQuery.options.skip)
       .limit(parsedQuery.options.limit)
-      // .skip(parseInt(skip))
-      // .limit(parse(limit))
-      // .sort({[sort]: -1}) // or: .sort({[sort]:1})
       
-      res.send(usersList)
-      //http://localhost:3022/users?name=Alina
-      //http://localhost:3022/users?age=26
-      // mongoDB: {"age": {$gt:26}}
-  } catch(error){
-    next(error)
-  }
-  
+      res.status(200).send(usersList)
+   } catch(error){
+       next(error)
+   }
 })
 
 
 
-// 2. GET one:
+// 2.GET only one user: //http://localhost:3022/users/5f0582f4190cd50454c37533
 usersRouter.get("/:id", async (req, res, next) => {
   try{
-     const user =   await UserSchema.findById(req.params.id)
-     if(user){res.send(user)}
-     else {
-       const error = new Error()
-       error.httpStatusCode = 404
-       next(error)
-     }
-     console.log(user)
-     
-     // if we type an id that doesnt exists we get an error : "Cast to ObjectId failed for value "5f04d25ca5688104a848d0d823" at path "_id" for model "user""
-  } catch(error){
-    next(error)
-  }
-
+        const user =   await UserSchema.findById(req.params.id)
+    if(user)
+        {res.send(user)}
+    else{
+         const error = new Error()
+         error.httpStatusCode = 404
+         next(error)
+       }
+        console.log(user)
+  }catch(error){
+     next(error)
+   }
 })
 
 
@@ -58,16 +54,12 @@ usersRouter.get("/:id", async (req, res, next) => {
 // 3. POST:
 usersRouter.post("/", async (req, res, next) => {
   try{
-
-    const newUser = new UserSchema(req.body)
-    const response = await newUser.save()
-    res.send(response)
-
+      const newUser = new UserSchema(req.body)
+      const response = await newUser.save()
+      res.send(response)
   } catch(error){
     next(error)
-
   }
-  
 })
 
 
@@ -75,14 +67,12 @@ usersRouter.post("/", async (req, res, next) => {
 // 4. PUT:
 usersRouter.put("/:id", async (req, res, next) => {
   try{
-    const user =    await UserSchema.findByIdAndUpdate(req.params.id, req.body)
-    console.log(user)
-    res.send(user)
-  }catch(error){
+     const user =  await UserSchema.findByIdAndUpdate(req.params.id, req.body)
+      console.log(user)
+      res.send(user)
+   }catch(error){
     next(error)
-
-  }
-
+   }
 })
 
 
@@ -95,8 +85,7 @@ usersRouter.delete("/:id", async (req, res, next) => {
     res.send("Deleted!")
   } catch(error){
     next(error)
-  }
-  
+  } 
 })
 
 module.exports = usersRouter
