de75d1be4a45ee9c43526325d913e196e833766f
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-30 00:33:51 +0200

    Update index.js

--- Modul-6/m6-w14-d2/src/services/books/index.js
+++ Modul-6/m6-w14-d2/src/services/books/index.js
@@ -4,36 +4,38 @@ const BookSchema = require("./schema")
 
 const booksRouter = express.Router()
 
+// Get all books:  //
 booksRouter.get("/", async (req, res, next) => {
   try {
-    const books = await BookSchema.find(req.query)
-    res.send(books)
-  } catch (error) {
-    next(error)
-  }
+      const books = await BookSchema.find(req.query)
+      res.send(books)
+   } catch (error) {
+       next(error)
+   }
 })
 
+
+// Get only one book:  //
 booksRouter.get("/:asin", async (req, res, next) => {
   try {
-    // const id = req.params.id
-    // const book = await BookSchema.findById(id)
-    const book = await BookSchema.findOne({_id: req.params.id})
-    if (book) {
-      res.send(book)
-    } else {
-      const error = new Error()
-      error.httpStatusCode = 404
-      next(error)
-    }
-  } catch (error) {
-    console.log(error)
-    next("While reading books list a problem occurred!")
+        const book = await BookSchema.findOne({_id: req.params.asin})
+     if (book) {
+        res.send(book)
+     }else {
+        const error = new Error()
+        error.httpStatusCode = 404
+        next(error)
+     }
+  }catch (error) {
+      console.log(error)
+      next("While reading books list a problem occurred!")
   }
 })
 
+
+// POST a new book:  //
 booksRouter.post("/", async (req, res, next) => {
   try {
-
     const newbook = new BookSchema(req.body)
     //const { _id } = await newbook.save()
     const response = await newbook.save()
@@ -43,10 +45,12 @@ booksRouter.post("/", async (req, res, next) => {
   }
 })
 
+
+// PUT - Update a specific book:  //
 booksRouter.put("/:asin", async (req, res, next) => {
   try {
     // const book = await BookSchema.findByIdAndUpdate(req.params.id, req.body)
-    const book = await BookSchema.findOneAndUpdate({_id:req.params.asin}, req.body)
+    const book = await BookSchema.findOneAndUpdate({_id: req.params.asin}, req.body)
     console.log(book)
     if (book) {
       res.send("Ok")
@@ -60,23 +64,12 @@ booksRouter.put("/:asin", async (req, res, next) => {
   }
 })
 
-booksRouter.delete("/:id", async (req, res, next) => {
-  // try {
-  //   const book = await BookSchema.findByIdAndDelete(req.params.id)
-  //   if (book) {
-  //     res.send("Deleted")
-  //   } else {
-  //     const error = new Error(`book with id ${req.params.id} not found`)
-  //     error.httpStatusCode = 404
-  //     next(error)
-  //   }
-  // } catch (error) {
-  //   next(error)
-  // }
 
+// DELETE a specific book:  //
+booksRouter.delete("/:asin", async (req, res, next) => {
   try{
     await BookSchema.findOneAndDelete({_id: req.params.asin})
-    res.send("Deleted!")
+     res.send("Deleted!")
   }catch(error) {
     next(error)
   }
