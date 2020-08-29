1fa58cbb4adefe8f3fa25bba6cd6b5f9e5363299
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-30 00:26:49 +0200

    Update Reservations.jsx

--- Modul-4/M4-W9-D3/strivesturap/src/components/Reservations.jsx
+++ Modul-4/M4-W9-D3/strivesturap/src/components/Reservations.jsx
@@ -11,17 +11,17 @@ class Reservation extends React.Component {
             <div className='mt-2'>
                 <ListGroup> 
                     {
-                    this.state.reservations.length >0 && this.state.reservations.map((reservation, i) =>
-    
+                    this.state.reservations.length>0 && this.state.reservations.map((reservation, i) =>
+                      {
                         return(
                             <ListGroup.Item key={i}>
                                 From: {reservation.name}, 
                                 for {reservation.numberOfPersons},
                                 at {reservation.dateTime}
                             </ListGroup.Item>
-                        )
-                          }  )
-                   })
+                              )
+                         } )
+                   }
                 </ListGroup>
                 {this.state.reservations.length === 0 && (<div>No reservations! </div>)}
             </div>
