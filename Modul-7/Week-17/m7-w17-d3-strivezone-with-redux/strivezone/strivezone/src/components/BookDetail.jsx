5f2f86e1f9ae59b6f01da2410060c1849edcfb72
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-09-30 21:42:57 +0200

    Create BookDetail.jsx

---
+++ Modul-7/Week-17/m7-w17-d3-strivezone-with-redux/strivezone/strivezone/src/components/BookDetail.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { books } from "../data/books";
+import Button from "react-bootstrap/Button"
+import {connect} from "react-redux"
+
+
+
+const mapStateToProps = (state) => state;
+
+
+const mapDispatchToProps = dispatch => ({
+    addToCart: (id) => dispatch({
+        type: "ADD_ITEM_TO_CART",
+        payload: id
+    })
+})
+
+
+
+class BookDetail extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = { book: null }
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.bookSelected !== this.props.bookSelected){
+            this.setState({ book: this.props.books.find( book => 
+                 book.id === this.props.bookSelected)})
+        }
+    }
+
+    render(){
+        return this.state.book  ?( 
+        <div className="col-sm-8">
+                <div className="row mt-3">
+                    <div className="col-sm-12">
+                    <h1>{this.state.book.title}</h1>
+                    </div>
+                <div className="col-sm-5">
+                   <img className="book-cover" 
+                        src={this.state.book.imageUrl} 
+                        alt="book Selected" />
+                </div>
+                <div className="col-sm-7">
+                   <p><span         className="font-weight-bold">Description: </span>
+                    {this.state.book.description}
+                   </p>
+                    <p><span className="font-weight-bold">Price: </span>
+                    {this.state.book.price} </p>
+                   
+                    <Button color="primary" 
+                    onClick={() =>this.props.addToCart(this.state.book.id)}>Buy</Button>
+                </div>
+                </div>
+              </div> 
+            ): (<div className="col-sm-8">
+                <div className="row margin-top">
+                    <h3> Please Select a book! </h3>
+                </div>
+            </div>)
+            
+        
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BookDetail);
